7c81d929afbbb450fa45e3c58b165ce3cadb6814
PedroRaingo <pedroraingo@gmail.com>
2025-04-09 15:37:36 -0300

    Update ingreso-permitido.py

--- ingreso-permitido.py
+++ ingreso-permitido.py
@@ -1,8 +1,5 @@
 # Paso 1: Solicitar al usuario que ingrese la edad del cliente
 
-from operator import truediv
-
-
 edad = int(input("Por favor ingrese la edad del cliente: "))
 
 # Paso 2: Verificar si la edad ingresada cumple con el requisito para ingresar a la discoteca
